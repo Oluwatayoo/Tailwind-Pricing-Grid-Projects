a3f32e836b7e6a906410488eaee5f002f70f5a9d
Oluwatayo <ibitoyeoluwatayo5@gmail.com>
2025-10-01 21:04:21 +0100

    Initial commit: added 6 Tailwind CSS Pricing Cards mini Projects

---
+++ Pricing Grid 3/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "Poppins",
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          "Apple Color Emoji",
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+"# Pricing Grids with Tailwind CSS" 
